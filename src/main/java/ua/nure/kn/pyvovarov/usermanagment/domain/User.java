c09b2436530d72abae7d84d6f05999740d980bce
vladyslav.hryhoriev@spryrocks.com <vladyslav.hryhoriev@spryrocks.com>
2019-10-09 23:25:24 +0300

    complete Test

---
+++ src/main/java/ua/nure/kn/pyvovarov/usermanagment/domain/User.java
@@ -0,0 +1,82 @@
+package ua.nure.kn.pyvovarov.usermanagment.domain;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+public class User implements Serializable {
+
+
+    private static final long serialVersionUID = -138019132080436045L;
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public long getAge() {
+    	Calendar calendar = Calendar.getInstance();
+    	calendar.setTime(new Date());
+    	int currentYear = calendar.get(Calendar.YEAR);
+    	int currentMonth = calendar.get(Calendar.MONTH);
+    	int currentDay = calendar.get(Calendar.DATE);
+    
+    	calendar.setTime(getDateOfBirth());
+    	
+    	int birthYear = calendar.get(Calendar.YEAR);
+    	int birthMonth = calendar.get(Calendar.MONTH);
+    	int birthDay = calendar.get(Calendar.DATE);
+    	long userAge = currentYear - birthYear;
+    	if(userAge >= 0) {
+    		if(currentMonth - birthMonth < 0) {
+    			return --userAge;
+    		}
+    		else if (currentMonth - birthMonth == 0) {
+    			if(currentDay - birthDay >= 0) {
+    				return userAge;
+    			}
+    			else {
+    				return --userAge;
+    			}
+    		}
+    	}
+    	else if(userAge < 0) {
+    		return -1;
+    	}
+    	return userAge;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFullName() {
+        return getFirstName() + " " + getLastName();
+    }
+}
---
+++ src/test/java/ua/nure/kn/pyvovarov/usermanagment/domain/UserTest.java
@@ -0,0 +1,137 @@
+package ua.nure.kn.pyvovarov.usermanagment.domain;
+import junit.framework.TestCase;
+import ua.nure.kn.pyvovarov.usermanagment.domain.User;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class UserTest extends TestCase {
+  
+    public static final int CURRENT_YEAR = 2019;
+
+    //Test case ¹ 1 if the birth day in the next month of the year
+    public static final int YEAR_OF_BIRTH1 = 2000;
+    public static final int MONTH_OF_BIRTH1 = Calendar.DECEMBER;
+    public static final int DATE_OF_BIRTH1 = 3;
+    public static final int ETALONE_AGE1 = CURRENT_YEAR - YEAR_OF_BIRTH1 - 1;
+
+    // Test case ¹ 2 if the birth day is today
+    public static final int YEAR_OF_BIRTH2 = 1999;
+    public static final int MONTH_OF_BIRTH2 = Calendar.AUGUST;
+    public static final int DATE_OF_BIRTH2 = 14;
+    public static final int ETALONE_AGE2 = CURRENT_YEAR - YEAR_OF_BIRTH2 ;
+
+    // Test case ¹ 3 if the birth day is this month but DayOfBirth later than CurrentDay
+    public static final int YEAR_OF_BIRTH3 = 1998;
+    public static final int MONTH_OF_BIRTH3 = Calendar.OCTOBER;
+    public static final int DATE_OF_BIRTH3 = 15;
+    public static final int ETALONE_AGE3 = CURRENT_YEAR - YEAR_OF_BIRTH3 -1 ;
+
+    // Test case ¹ 4 if the birth day is this month but DayOfBirth earlier than CurrentDay
+    public static final int YEAR_OF_BIRTH4 = 2000;
+    public static final int MONTH_OF_BIRTH4 = Calendar.SEPTEMBER;
+    public static final int DATE_OF_BIRTH4 = 15;
+    public static final int ETALONE_AGE4 = CURRENT_YEAR - YEAR_OF_BIRTH4;
+
+    // Test case ¹ 5 if the birthday has passed
+    public static final int YEAR_OF_BIRTH5 = 1980;
+    public static final int MONTH_OF_BIRTH5 = Calendar.AUGUST;
+    public static final int DATE_OF_BIRTH5 = 20;
+    public static final int ETALONE_AGE5 = CURRENT_YEAR - YEAR_OF_BIRTH5;
+
+    // Test case # 6 if the person was not born and birthYear differs from currentYear
+    public static final int YEAR_OF_BIRTH6 = 2050;
+    public static final int MONTH_OF_BIRTH6 = Calendar.MARCH;
+    public static final int DATE_OF_BIRTH6 = 10;
+    public static final int ETALONE_AGE6 = -1;
+  
+    // Test case # 7 if the person was not born and birthYear equals to currentYear
+    public static final int YEAR_OF_BIRTH7 = CURRENT_YEAR;
+    public static final int MONTH_OF_BIRTH7 = Calendar.OCTOBER;
+    public static final int DATE_OF_BIRTH7 = 11;
+    public static final int ETALONE_AGE7 = -1;
+    
+    private User user;
+    private Date dateOfBirth;
+
+    public void testGetFullName() {
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        assertEquals("John Doe", user.getFullName());
+    }
+
+   
+    // Test case #1
+     public void testGetAge1() { 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH1, MONTH_OF_BIRTH1, DATE_OF_BIRTH1);
+        dateOfBirth = calendar.getTime();
+        user.setDateOfBirth(dateOfBirth);
+        assertEquals(ETALONE_AGE1, user.getAge());
+    }
+
+     // Test case #2
+    public void testGetAge2() { 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH2, MONTH_OF_BIRTH2, DATE_OF_BIRTH2);
+        dateOfBirth = calendar.getTime();
+        user.setDateOfBirth(dateOfBirth);
+        assertEquals(ETALONE_AGE2, user.getAge());
+    }
+
+    // Test case #3
+    public void testGetAge3() { 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH3, MONTH_OF_BIRTH3, DATE_OF_BIRTH3);
+        dateOfBirth = calendar.getTime();
+        user.setDateOfBirth(dateOfBirth);
+        assertEquals(ETALONE_AGE3, user.getAge());
+    }
+
+    // Test case #4
+    public void testGetAge4() { 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH4, MONTH_OF_BIRTH4, DATE_OF_BIRTH4);
+        dateOfBirth = calendar.getTime();
+        user.setDateOfBirth(dateOfBirth);
+        assertEquals(ETALONE_AGE4, user.getAge());
+    }
+
+    // Test case #5
+    public void testGetAge5() { 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH5, MONTH_OF_BIRTH5, DATE_OF_BIRTH5);
+        dateOfBirth = calendar.getTime();
+        user.setDateOfBirth(dateOfBirth);
+        assertEquals(ETALONE_AGE5, user.getAge());
+    }
+    
+    // Test case #6
+    public void testGetAge6() { 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH6, MONTH_OF_BIRTH6, DATE_OF_BIRTH6);
+        dateOfBirth = calendar.getTime();
+        user.setDateOfBirth(dateOfBirth);
+        assertEquals(ETALONE_AGE6, user.getAge());
+    }
+    
+    // Test case #7
+    public void testGetAge7() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH7, MONTH_OF_BIRTH7, DATE_OF_BIRTH7);
+        dateOfBirth = calendar.getTime();
+        user.setDateOfBirth(dateOfBirth);
+        assertEquals(ETALONE_AGE7, user.getAge());
+    }
+    
+    protected void setUp() throws Exception {
+        super.setUp();
+        user = new User();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        user = new User();
+    }
+
+}
