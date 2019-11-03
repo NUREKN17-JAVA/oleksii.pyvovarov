fb45cdc3c8d758a436ace8113c22da595d879bcc
nureOleksiiPyvovarov <oleksii.pyvovarov@nure.ua>
2019-11-03 14:36:04 +0200

    Create DB connetction

---
+++ src/main/java/ua/nure/kn/pyvovarov/db/ConnectionFactoryImpl.java
@@ -0,0 +1,29 @@
+package ua.nure.kn.pyvovarov.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactoryImpl implements ConnectionFactory {
+
+	 private String url = "jdbc:hsqldb:file:/db/usermanagment";
+	    private String user = "sa";
+	    private String password = "";
+	    private String driver = "org.hsqldb.jdbcDrive";
+	
+	@Override
+	public Connection createConnection() throws DataBaseException {
+		 try {
+	            Class.forName(driver);
+	        } catch (ClassNotFoundException e) {
+	            throw new RuntimeException();
+	        }
+	        try {
+	            return DriverManager.getConnection(url, user, password);
+	        } catch (SQLException e) {
+	            throw new DataBaseException(e);
+	        }
+	    }
+	}
+
+}
