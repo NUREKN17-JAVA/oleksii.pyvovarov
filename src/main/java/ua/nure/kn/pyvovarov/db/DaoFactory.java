73dcef634e3fff983f9c5796cf438e787efdb939
nureOleksiiPyvovarov <oleksii.pyvovarov@nure.ua>
2019-11-03 23:10:21 +0200

    Complete getUserDao method

--- src/main/java/ua/nure/kn/pyvovarov/db/DaoFactory.java
+++ src/main/java/ua/nure/kn/pyvovarov/db/DaoFactory.java
@@ -5,28 +5,40 @@ import java.util.Properties;
 
 public class DaoFactory {
 	 private final Properties properties;
-
+	 private static final String PROPERTIES = "settings.properties";
+	 private static final String USER = "connection.user";
+	 private static final String PASSWORD = "connection.password";
+	 private static final String URL = "connection.url";
+	 private static final String DRIVER = "connection.driver";
+	 private static final String HSQLDB_USER_DAO = "ua.nure.kn.pyvovarov.db.HsqldbUserDao";
+	    
 	    public DaoFactory() {
 	        this.properties = new Properties();
 	        try {
 	            properties.load(getClass().getClassLoader()
-	                                      .getResourceAsStream("settings.properties"));
+	            	    .getResourceAsStream(PROPERTIES));
 	        } catch (IOException e) {
 	            throw new RuntimeException(e);
 	        }
 	    }
-	    
-	    private ConnectionFactory createConnection () {
-	        String user = properties.getProperty("connection.user");
-	        String password = properties.getProperty("connection.password");
-	        String url = properties.getProperty("connection.url");
-	        String driver = properties.getProperty("connection.driver");
+	    private ConnectionFactory createConnection() {
+	    	    String user = properties.getProperty(USER);
+	    	    String password = properties.getProperty(PASSWORD);
+	    	    String url = properties.getProperty(URL);
+	    	    String driver = properties.getProperty(DRIVER);
 
 	        return new ConnectionFactoryImpl(user, password, url, driver);
 	    }
 
-	    public Dao getDao () {
+	    public Dao getDao() throws ReflectiveOperationException {
 	        Dao result = null;
+	        try {
+	            Class hsqldbUserDaoClass = Class.forName(properties.getProperty(HSQLDB_USER_DAO));
+	            Dao hsqldbUserDao = (Dao) hsqldbUserDaoClass.newInstance();
+	            hsqldbUserDao.setConnectionFactory(createConnection());
+	        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+	            throw new ReflectiveOperationException(e);
+	        }
 	        return result;
 	    }
 }
