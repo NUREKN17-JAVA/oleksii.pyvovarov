fad5103c90b9ae1406a02cb80c3f17e00791837d
nureOleksiiPyvovarov <oleksii.pyvovarov@nure.ua>
2019-11-03 14:51:20 +0200

    Adding a user record to the database

--- src/main/java/ua/nure/kn/pyvovarov/db/HsqldbUserDao.java
+++ src/main/java/ua/nure/kn/pyvovarov/db/HsqldbUserDao.java
@@ -2,6 +2,10 @@ package ua.nure.kn.pyvovarov.db;
 
 import ua.nure.kn.pyvovarov.usermanagment.domain.User;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 
 public class HsqldbUserDao implements Dao<User> {
@@ -11,10 +15,27 @@ public class HsqldbUserDao implements Dao<User> {
 	    public HsqldbUserDao(ConnectionFactory factory) {
 	        connectionFactory = factory;
 	    }
-	    
+	    public static final String INSERT_QUERY = "INSERT INTO users " +
+	            "(firstname, lastname, dateofbirth VALUES (?,?,?))"; 
     @Override
     public User create(User entity) throws DataBaseException {
-        return null;
+    	 try {
+             Connection connection = connectionFactory.createConnection();
+             PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+             statement.setString(1, entity.getFirstName());
+             statement.setString(2, entity.getLastName());
+             statement.setDate(3, new Date(entity.getDateOfBirth()
+                                                 .getTime()));
+             int numberOfRows = statement.executeUpdate();
+             if (numberOfRows != 1) {
+                 throw new DataBaseException("Number of inserted rows: " + numberOfRows);
+             }
+             return null;
+         } catch (DataBaseException e) {
+             throw e;
+         } catch (SQLException e) {
+             throw new DataBaseException(e);
+         }
     }
 
     @Override
