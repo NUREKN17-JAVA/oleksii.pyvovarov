2d9f513fd4a0c8bdacf8dc5f676da32e241d3717
nureOleksiiPyvovarov <oleksii.pyvovarov@nure.ua>
2019-12-23 01:34:18 +0200

    Update BrowseServlet

--- src/main/java/ua/nure/kn/pyvovarov/web/BrowseServlet.java
+++ src/main/java/ua/nure/kn/pyvovarov/web/BrowseServlet.java
@@ -14,9 +14,75 @@ import ua.nure.kn.pyvovarov.db.DataBaseException;
 
 public class BrowseServlet extends HttpServlet {
 
+	private static final String ADD = "/add";
+	private static final String EDIT = "/edit";
+	private static final String DELETE = "/delete";
+	private static final String ERROR_ATTRIBUTE = "error";
+	private static final String DETAILS = "/details";
+	private static final String DETAILS_BUTTON = "detailsButton";
+	private static final String DELETE_BUTTON = "deleteButton";
+	private static final String EDIT_BUTTON = "editButton";
+	private static final String ADD_BUTTON = "addButton";
 	private static final String BROWSE_JSP = "/browse.jsp";
 	private static final String USERS_ATTRIBUTE = "users";
 
+	@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter(ADD_BUTTON) != null) {
+            add(req, resp);
+        } else if (req.getParameter(EDIT_BUTTON) != null) {
+            edit(req, resp);
+        } else if (req.getParameter(DELETE_BUTTON) != null) {
+            delete(req, resp);
+        } else if (req.getParameter(DETAILS_BUTTON) != null) {
+            details(req, resp);
+        } else {
+            browse(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        browse(req, resp);
+    }
+
+    private void details(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (prepareAnotherUserSrvlet(req, resp)) return;
+        req.getRequestDispatcher(DETAILS).forward(req, resp);
+    }
+
+    private boolean prepareAnotherUserSrvlet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idStr = req.getParameter("id");
+        if (idStr == null || idStr.trim().length() == 0) {
+            req.setAttribute(ERROR_ATTRIBUTE, "You must select a user");
+            req.getRequestDispatcher(BROWSE_JSP).forward(req, resp);
+            return true;
+        }
+        try {
+            User user = DaoFactory.getInstance().getUserDao().find(new Long(idStr));
+            req.getSession().setAttribute("user", user);
+        } catch (Exception e) {
+            req.setAttribute(ERROR_ATTRIBUTE, "ERROR:" + e.toString());
+            req.getRequestDispatcher(BROWSE_JSP).forward(req, resp);
+            return true;
+        }
+        return false;
+    }
+
+    private void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (prepareAnotherUserSrvlet(req, resp)) return;
+        req.getRequestDispatcher(DELETE).forward(req, resp);
+    }
+
+    private void edit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (prepareAnotherUserSrvlet(req, resp)) return;
+        req.getRequestDispatcher(EDIT).forward(req, resp);
+    }
+
+    private void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(ADD).forward(req, resp);
+    }
+	
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			browse(req, resp);
 	}
