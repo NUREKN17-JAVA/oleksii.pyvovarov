671bbc5d8be219e98db16d485ce9195bb76c9a6a
nureOleksiiPyvovarov <oleksii.pyvovarov@nure.ua>
2019-12-28 02:46:27 +0200

    Create response message

--- src/main/java/ua/nure/kn/pyvovarov/agent/RequestServer.java
+++ src/main/java/ua/nure/kn/pyvovarov/agent/RequestServer.java
@@ -1,9 +1,15 @@
 package ua.nure.kn.pyvovarov.agent;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.StringTokenizer;
+import org.hsqldb.lib.Iterator;
 
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import ua.nure.kn.pyvovarov.db.DaoFactory;
+import ua.nure.kn.pyvovarov.db.DataBaseException;
+import ua.nure.kn.pyvovarov.usermanagment.domain.User;
 
 public class RequestServer {
 
@@ -29,8 +35,30 @@ public class RequestServer {
 	}
 
 	private ACLMessage createReply(ACLMessage message) {
-		// TODO Auto-generated method stub
-		return null;
+		ACLMessage reply = message.createReply();
+
+		String content = message.getContent();
+		StringTokenizer tokenizer = new StringTokenizer(content, ",");
+		if (tokenizer.countTokens() == 2) {
+			String firstName = tokenizer.nextToken();
+			String lastName = tokenizer.nextToken();
+			Collection users = null;
+			try {
+				users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+			} catch (DataBaseException e) {
+				e.printStackTrace();
+				users = new ArrayList(0);
+			}
+
+			StringBuffer buffer = new StringBuffer();
+			for (Iterator it = users.iterator(); it.hasNext();) {
+				User user = (User) it.next();
+				buffer.append(user.getId()).append(",");
+				buffer.append(user.getFirstName()).append(",");
+				buffer.append(user.getLastName()).append(";");
+			}
+			reply.setContent(buffer.toString());
+		}		return null;
 	}
 
 	
